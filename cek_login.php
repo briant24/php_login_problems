195e08def1cc629ab00837e2f3ca7a3c8f3fb3e6
briant24 <briantadr24@gmail.com>
2022-12-26 02:37:35 +0700

    Limit login attemps

--- cek_login.php
+++ cek_login.php
@@ -2,43 +2,59 @@
 include "koneksi.php";
 
 session_start();
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = mysqli_query($koneksi, "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'");
+$time=time()-30;
+$ip_address=getIpAddr();
+$query=mysqli_query($koneksi,"SELECT COUNT(*) as total_count from login_logs where try_times > $time and ip_address='$ip_address'");
+$check_login_row=mysqli_fetch_assoc($query);
+$total_count=$check_login_row['total_count'];
+if($total_count==3){
+    echo "<script>alert('Terlalu banyak percobaan, coba lagi dalam 30 detik!'); window.location = 'index.php'</script>";
+}else{
+    $query = mysqli_query($koneksi, "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'");
 
-$cek = mysqli_num_rows($query);
+    $cek = mysqli_num_rows($query);
+    
+    $r = mysqli_fetch_array($query);
 
-$r = mysqli_fetch_array($query);
+    if ($cek > 0) {
 
-if ($cek > 0) {
+        $_SESSION['namaadmin']  = $r['nama_admin'];
+        $_SESSION['username']   = $r['username'];
+        $_SESSION['password']   = $r['password'];
+        $_SESSION['idadmin']    = $r['id_admin'];
 
-    $_SESSION['namaadmin']  = $r['nama_admin'];
-    $_SESSION['username']   = $r['username'];
-    $_SESSION['password']   = $r['password'];
-    $_SESSION['idadmin']    = $r['id_admin'];
-
-    if (!empty($_POST["remember"])) {
-        setcookie("username", $_POST["username"], time() + (60 * 60 * 24 * 5));
-        setcookie("password", $_POST["password"], time() + (60 * 60 * 24 * 5));
+        if (!empty($_POST["remember"])) {
+            setcookie("username", $_POST["username"], time() + (60 * 60 * 24 * 5));
+            setcookie("password", $_POST["password"], time() + (60 * 60 * 24 * 5));
+        } else {
+            setcookie("username", "");
+            setcookie("password", "");
+        }
+        mysqli_query($koneksi,"DELETE from login_logs WHERE ip_address='$ip_address'");
+        header("location:dashboard.php?hal=home");
     } else {
-        setcookie("username", "");
-        setcookie("password", "");
+        $total_count++;
+        $rem_attm=3-$total_count;
+        if($rem_attm==0){
+            echo "<script>alert('Terlalu banyak percobaan, coba lagi dalam 30 detik!'); window.location = 'index.php'</script>";
+        }else{
+            echo "<script>alert('Password atau username salah! Sisa $rem_attm percobaan lagi!');window.location = 'index.php'</script>";
+        }
+        $try_time=time();
+        mysqli_query($koneksi,"INSERT into login_logs values('','$ip_address','$try_time')");
     }
-
-    header("location:dashboard.php?hal=home");
-} else {
-    echo'
-    <center>
-    <br><br><br><br><br><br><br><br><br><br>
-    <b>Username atau Password Salah!</b><br><br>
-    <b>atau</b><br>
-    <b>Akun anda telah diblokir</b><br>
-
-    <a href="index.php" title="Klik Gambar ini untuk Kembali ke Halaman Login"><img src="img/key-login.png" height="100" width="100"></img></a>
-    </center>
-    ';
 }
-
+function getIpAddr(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+    $ipAddr=$_SERVER['HTTP_CLIENT_IP'];
+    }elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+    $ipAddr=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+    $ipAddr=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ipAddr;
+}
 ?>
